3647ebfee9e7ffcec6af2f7ed224f09d67ed986c
HarshPatel94264 <harshribadiya13@gmail.com>
2023-12-24 00:01:04 +0530

    version 3

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000;
+
+const views_dir = path.join(__dirname,'./views') 
+const publicpath = path.join(__dirname,'./Public')
+app.use(express.static(publicpath))
+
+app.set('view engine','ejs')
+app.set('views',views_dir)
+
+app.get('/',(req,res)=>{
+    res.render('index')
+}) 
+app.get('/weather',(req,res)=>{
+    res.render('weather')
+})
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+app.get('*',(req,res)=>{
+    res.render('404')
+})
+
+app.listen(port,(err)=>{
+    console.log(`server running`)
+})
+
+
+
